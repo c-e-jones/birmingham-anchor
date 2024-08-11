9213d88dbe353fac23b9928ea1bbc865ea57e274
Carl Jones <saanotherperson@gmail.com>
2024-08-11 15:08:00 +0000

    adding basic auth functionality

--- birmingham_anchor/settings.py
+++ birmingham_anchor/settings.py
@@ -153,3 +153,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend'
+]
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_REQUIRED = True
