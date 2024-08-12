3ea103f7b9ebd5ba722ad0b5b4808154236be4a7
Carl Jones <saanotherperson@gmail.com>
2024-08-12 22:01:56 +0000

    removed error from views.py, left old code in imports

--- organisations/views.py
+++ organisations/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Event
-
+from django.views.generic import TemplateView
 
 # Create your views here.
 
