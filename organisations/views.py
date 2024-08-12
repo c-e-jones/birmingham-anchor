c4ab1ae268148ff7b7414203ed5877e1fdcc5805
Carl Jones <saanotherperson@gmail.com>
2024-08-12 21:59:05 +0000

    adding urls and views

--- organisations/views.py
+++ organisations/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+from .models import Event
+
 
 # Create your views here.
 
@@ -8,3 +10,7 @@ class HomePage(TemplateView):
     Displays home page "
     """ 
     template_name = 'index.html'
+
+
+class EventList(generic.EventView):
+    model = Event
