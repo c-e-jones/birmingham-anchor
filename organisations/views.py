f309a8d3eebc594b752b0ef235e355289b9ca267
Carl Jones <saanotherperson@gmail.com>
2024-08-13 16:38:31 +0000

    forgot comma in views

--- organisations/views.py
+++ organisations/views.py
@@ -10,4 +10,4 @@ class EventList(generic.ListView):
 def frontpage(request):
     events = Event.objects.all
     
-    return render(request, '/event_list.html' {'events': events})
+    return render(request, '/event_list.html', {'events': events})
