f1d853b669a995b1abfaf6e22b18824384daa6b0
Carl Jones <saanotherperson@gmail.com>
2024-08-13 17:40:19 +0000

    reverting way back to just event lists

--- organisations/views.py
+++ organisations/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from django.views import generic
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_list_or_404, render, redirect
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView
 from organisations.models import Event
 
 # Create your views here.
 
 class EventList(generic.ListView):
-    model = Event
-
-def frontpage(request):
-    events = Event.objects.all
-    context = {'events': events}
-    return render(request, '/event_list.html', context)
+    queryset = Event.objects.all()
+    template_name = "event_list.html"
